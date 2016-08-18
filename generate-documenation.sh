f8eae25821e83e18178cc2a9aeae694cbe9d9417
james2doyle <james2doyle@gmail.com>
2016-08-18 14:20:57 -0700

    improve generate script

--- generate-documenation.sh
+++ generate-documenation.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-apigen generate --source invoke-helpers.php --destination api --template-theme bootstrap
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if command_exists apigen ; then
+    apigen generate --source invoke-helpers.php --destination api --template-theme bootstrap
+else
+    echo "You need to install apigen to build the docs. Please visit http://www.apigen.org/"
+    exit 1
+fi
+
