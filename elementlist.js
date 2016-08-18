aebddc38910e1d7911b5e5da34fc97147798d16d
james2doyle <james2doyle@gmail.com>
2016-08-18 14:22:21 -0700

    add api docs

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","asset()"],["f","auth()"],["f","content()"],["f","copyright_date()"],["f","dd()"],["f","e()"],["f","featured_image()"],["f","is_even()"],["f","is_odd()"],["f","plural_count()"],["f","request()"],["f","str_limit()"],["f","str_slug()"],["f","strip_non_numeric()"],["f","template_is()"],["f","url()"],["f","var_template_include()"],["f","view()"],["f","word_limit()"]];
