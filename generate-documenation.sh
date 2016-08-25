58f80fe6036f3b6d88b9b5dcd544634685994004
james2doyle <james2doyle@gmail.com>
2016-08-25 09:49:45 -0700

    add the deploy to the generate docs function

--- generate-documenation.sh
+++ generate-documenation.sh
@@ -6,6 +6,8 @@ command_exists () {
 
 if command_exists apigen ; then
     apigen generate --source invoke-helpers.php --destination api --template-theme bootstrap
+    # push to the /api folder to the gh-pages branch
+    git subtree push --prefix api origin gh-pages
 else
     echo "You need to install apigen to build the docs. Please visit http://www.apigen.org/"
     exit 1
